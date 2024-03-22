224b6ed90fde1d94100d9abf5b4645562cf1f20f
Cris P <criscodex@users.noreply.github.com>
2024-03-21 21:48:35 -0700

    Command injection mitigation: sanitize input
    To mitigate the command injection vulnerability, I'm sanitizing the input by stripping everything except for alphanumeric characters

--- vuln-1.py
+++ vuln-1.py
@@ -1,5 +1,6 @@
 import os
 import flask
+import re
 
 app = flask.Flask(__name__)
 
@@ -7,6 +8,9 @@ app = flask.Flask(__name__)
 @app.route("/route_param/<route_param>")
 def route_param(route_param):
 
+    # sanitize input: allow only alphanumeric characters
+    re.sub(r'[^a-zA-Z0-9]', '', route_param)
+    
     # ruleid:dangerous-os-exec
     os.execl("/bin/bash", "/bin/bash", "-c", route_param)
 
